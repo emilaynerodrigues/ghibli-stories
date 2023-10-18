5066bf1dd2c18ab722c2d12cd94146f82a38bbc8
emilaynerodrigues <emilaynx@gmail.com>
2023-10-17 22:42:58 -0400

    subindo arquivos de ghibli stories

---
+++ script/main.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+  pagination: {
+    clickable: true,
+  },
+});
+
+//selecionando botões com class movie
+const movie = document.querySelectorAll(".movie");
+
+//selecionando movie-info
+const movieInfo = document.querySelectorAll(".movie-info");
+
+//percorrendo cada button
+movie.forEach((button, index) => {
+  //disparando a ação - é executado quando o button é clicado
+  button.addEventListener("click", () => {
+    removeMovieInfoClass();
+
+    //adicionando class active no personagem
+    movieInfo[index].classList.add("active");
+  });
+});
+
+//verificando se há algum botão selecionado e removendo a seleção
+function removeMovieInfoClass() {
+  const movieActive = document.querySelector(".movie-info.active");
+  movieActive.classList.remove("active");
+}
